514d0b2fae26da8300158dca425f96eb1c802449
Ken Holladay <Kenholladay@gmail.com>
2017-09-20 01:50:46 -0600

    Update page.tpl.php
    adding martech

--- public/themes/seven/page.tpl.php
+++ public/themes/seven/page.tpl.php
@@ -1,6 +1,6 @@
 
   <div id="branding" class="clearfix">
-    <?php print $martech: ?>
+    <?php print $martech2: ?>
     <?php print $breadcrumb; ?>
     <?php print render($title_prefix); ?>
     <?php if ($title): ?>
